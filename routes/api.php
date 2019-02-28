edfe4fefc81183d8c36879e6984acdee1d436b7d
leninsheikh <justtlenin@gmail.com>
2019-02-28 14:15:58 +0600

    laravel permission added

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 
 
-Route::post('login', 'AuthController@login');
+Route::post('login', 'AuthController@login')->name('login');
 Route::post('register', 'AuthController@signup');
 
 Route::group([
@@ -11,5 +11,28 @@ Route::group([
 ], function () {
     Route::get('logout', 'AuthController@logout');
     Route::get('user', 'AuthController@user');
+    Route::group(['middleware' => ['role:student']], function () {
+        Route::get('per', function () {
+            return 'ok';
+        });
+    });
+
 });
+
+
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+Route::get('rp', function () {
+//    $role = Role::create(['name' => 'student']);
+//    $role = Role::create(['name' => 'teacher']);
+//    $permission = Permission::create(['name' => 'student_basic']);
+//    $permission = Permission::create(['name' => 'teacher_basic']);
+//    return Role::all();
+    App\User::find(1)->assignRole('student');
+    return 'gg';
+});
+
 
